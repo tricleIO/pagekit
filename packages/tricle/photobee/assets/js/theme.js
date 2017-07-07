4f020366190699d5cce2e42f60bd5c0f585df991
Pavel Janku <pavel.janku@tricle.io>
2017-07-07 06:31:02 +0200

    modals

--- packages/tricle/photobee/assets/js/theme.js
+++ packages/tricle/photobee/assets/js/theme.js
@@ -1,6 +1,7 @@
 //import $ from "jquery";
 
 $(document).ready(function () {
+
     $(".lock-char").keyup(function () {
         console.log(this);
         if (this.value.length == this.maxLength) {
@@ -17,5 +18,23 @@ $(document).ready(function () {
         this.classList.toggle('open')
         document.getElementsByClassName('main-menu')[0].classList.toggle("open");
     };
+
+    $('.bee-modal-wrapper').hide();
+
+    $('main').on('click','.bee-item',function (e) {
+        let modalId = $(this).data('modal');
+        $('.bee-modal-wrapper').hide();
+        $('.bee-modal-wrapper#'+modalId).show();
+    });
+
+    $('main').on('click','.close-modal',function (e) {
+        $(this).closest('.bee-modal-wrapper').hide();
+    })
+
+    $('main').on('click','.modal-overlay',function (e) {
+        $(this).closest('.bee-modal-wrapper').hide();
+    })
+
+
 
 });
--- packages/tricle/photobee/js/theme.js
+++ packages/tricle/photobee/js/theme.js
@@ -54,6 +54,7 @@
 	/* WEBPACK VAR INJECTION */(function($) {//import $ from "jquery";
 	
 	$(document).ready(function () {
+	
 	    $(".lock-char").keyup(function () {
 	        console.log(this);
 	        if (this.value.length == this.maxLength) {
@@ -70,6 +71,24 @@
 	        this.classList.toggle('open')
 	        document.getElementsByClassName('main-menu')[0].classList.toggle("open");
 	    };
+	
+	    $('.bee-modal-wrapper').hide();
+	
+	    $('main').on('click','.bee-item',function (e) {
+	        let modalId = $(this).data('modal');
+	        $('.bee-modal-wrapper').hide();
+	        $('.bee-modal-wrapper#'+modalId).show();
+	    });
+	
+	    $('main').on('click','.close-modal',function (e) {
+	        $(this).closest('.bee-modal-wrapper').hide();
+	    })
+	
+	    $('main').on('click','.modal-overlay',function (e) {
+	        $(this).closest('.bee-modal-wrapper').hide();
+	    })
+	
+	
 	
 	});
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(2)))
